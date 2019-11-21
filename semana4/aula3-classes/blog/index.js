55c8ad76e55d7741ce40f9cbc227023570570a3d
eloisa-castro <elocastro31@gmail.com>
2019-11-21 13:01:41 -0300

    Atividade 1 - Passo 3

--- semana4/aula3-classes/blog/index.js
+++ semana4/aula3-classes/blog/index.js
@@ -6,11 +6,18 @@ class post {
     }
 }
 
+let arrayPosts = []
+
 function aoClicarCriarPost() {
     const tituloDoPost = document.getElementById("titulo-post")
     const autorDoPost = document.getElementById("autor-post")
     const conteudoDoPost = document.getElementById("conteudo-post")
     const novoPost = new post(tituloDoPost.value, autorDoPost.value, conteudoDoPost.value)
+    arrayPosts.push(novoPost)
+    tituloDoPost.value = ""
+    autorDoPost.value = ""
+    conteudoDoPost.value = ""
+    console.log(arrayPosts)
     /* console.log(novoPost.titulo)
     console.log(novoPost.autor)
     console.log(novoPost.conteudo) */
