69585fda5e35440b43ed1c23031d15b7383ebf21
eloisa-castro <elocastro31@gmail.com>
2020-02-23 18:34:48 -0300

    Resolução da atividade da semana15-aula2

---
+++ semana15/aula2/src/saltydish.ts
@@ -0,0 +1,16 @@
+import { Dish } from "./dish";
+
+export class SaltyDish extends Dish {
+  constructor(
+    price: number,
+    cost: number,
+    ingredients: string[],
+    timeToCook: number
+  ) {
+    super(price, cost, ingredients, timeToCook);
+  }
+
+  public cook(): void {
+    console.log("Starting Salty Dish")
+  }
+}
