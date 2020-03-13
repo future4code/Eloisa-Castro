b9da12740bf0d9bbc152acaf92952b05e0940824
eloisa-castro <elocastro31@gmail.com>
2020-03-12 21:29:08 -0300

    Resolução da issue 2

--- semana17/aula1/src/presentation/index.ts
+++ semana17/aula1/src/presentation/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { signupEndpoint } from "./endpoints/signup";
 import { getUserByEmail } from "./endpoints/getUserByEmail";
+import { getAllUsers } from "./endpoints/getAllUsers";
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,7 @@ app.use(express.json());
 app.post("/signup", signupEndpoint);
 
 app.get("/user", getUserByEmail);
+
+app.get("/user/all", getAllUsers);
 
 export default app;
