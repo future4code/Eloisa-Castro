f39f6cdc9824d4d411e6bb2bb21477c9232698e9
eloisa-castro <elocastro31@gmail.com>
2020-04-20 12:43:22 -0300

    Criação do uploadVideo (usecase e endpoint)

---
+++ semana21/backend/src/business/usecases/uploadVideo.ts
@@ -0,0 +1,48 @@
+import { AuthenticationGateway } from "../gateways/authenticationGateway";
+import { InvalidParameterError } from "../errors/InvalidParameterError";
+import { VideoGateway } from "../gateways/videoGateway";
+import { v4 } from "uuid";
+import { Video } from "../entities/video";
+
+export class UploadVideoUC {
+  constructor(
+    private videoGateway: VideoGateway,
+    private authenticationGateway: AuthenticationGateway
+  ) {}
+
+  public async execute(input: UploadVideoUCInput): Promise<UploadVideoUCOutput> { 
+    if (!input.title || !input.description || !input.video || !input.token) {
+      throw new InvalidParameterError("Invalid parameters")
+    }
+
+    const id = v4()
+
+    const tokenInfo = this.authenticationGateway.getUsersInfoFromToken(input.token)
+    const userId = tokenInfo.userId
+
+    const video = new Video(
+      id,
+      input.title,
+      input.description,
+      input.video,
+      userId
+    )
+
+    await this.videoGateway.saveVideo(video)
+
+    return {
+      message: "Successfully updated"
+    }
+  }
+}
+
+interface UploadVideoUCInput {
+  title: string;
+  description: string;
+  video: string;
+  token: string;
+}
+
+interface UploadVideoUCOutput {
+  message: string;
+}
--- semana21/backend/src/presentation/routes.ts
+++ semana21/backend/src/presentation/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { signupEndpoint } from "./endpoints/signup";
 import { loginEndpoint } from "./endpoints/login";
 import { changePasswordEndpoint } from "./endpoints/changePassword";
+import { uploadVideoEndpoint } from "./endpoints/uploadVideo";
 
 const app = express();
 app.use(express.json()); // Linha mágica (middleware)
@@ -9,5 +10,6 @@ app.use(express.json()); // Linha mágica (middleware)
 app.post('/signup', signupEndpoint)
 app.post('/login', loginEndpoint)
 app.post('/changePassword', changePasswordEndpoint)
+app.post('/uploadVideo', uploadVideoEndpoint)
 
 export default app;
